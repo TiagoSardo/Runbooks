d62093754950b984be092509582c22d4b90bb07d
TiagoSardo <87125698+TiagoSardo@users.noreply.github.com>
2021-08-25 09:23:15 +0100

    Add files via upload

---
+++ StopLifetime_VM.ps1
@@ -0,0 +1,34 @@
+#Connect to Azure
+################################################
+$connectionName = "AzureRunAsConnection"
+
+$servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+Connect-AzAccount -TenantId $servicePrincipalConnection.TenantID `
+    -ApplicationID $servicePrincipalConnection.ApplicationID `
+    -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+################################################
+
+#Define VM Tags
+################################################
+$vmStatus = "VM running"
+$tagValue1 = "Shd"
+$tagValue2 = "Arya Bera"
+$vmName = "vm-ppl-shd-uks-out-01"
+################################################
+
+#Select the VMs with the Tags defined previously
+################################################
+$mySelectedVMs = Get-AzVM -status | Where-Object {$_.PowerState -eq $vmStatus `
+    -and ($_.Name -eq $vmName)}
+################################################
+
+#Loop all the VMs that were found and turn them on
+################################################
+for ($i = 0; $i -lt $mySelectedVMs.Length; $i++) {
+    $resourceGroupName = $mySelectedVMs[$i].ResourceGroupName
+    $vmName = $mySelectedVMs[$i].Name
+
+    Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
+}
+################################################
---
+++ Stop_VM.ps1
@@ -0,0 +1,33 @@
+#Connect to Azure
+################################################
+$connectionName = "AzureRunAsConnection"
+
+$servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+Connect-AzAccount -TenantId $servicePrincipalConnection.TenantID `
+    -ApplicationID $servicePrincipalConnection.ApplicationID `
+    -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+################################################
+
+#Define VM Tags
+################################################
+$vmStatus = "VM running"
+$tagValue1 = "Shd"
+$tagValue2 = "Arya Bera"
+################################################
+
+#Select the VMs with the Tags defined previously
+################################################
+$mySelectedVMs = Get-AzVM -status | Where-Object {$_.PowerState -eq $vmStatus `
+    -and ($_.Tags.Values -eq $tagValue1 -and $_.Tags.Values -eq $tagValue2)}
+################################################
+
+#Loop all the VMs that were found and turn them on
+################################################
+for ($i = 0; $i -lt $mySelectedVMs.Length; $i++) {
+    $resourceGroupName = $mySelectedVMs[$i].ResourceGroupName
+    $vmName = $mySelectedVMs[$i].Name
+
+    Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
+}
+################################################
